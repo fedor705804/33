8742856b86e6ce2786b650ff284f043a4ae380cc
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-02-26 08:27:39 +0000

    олюнл

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-s = 'impossible'
-boss = s[7] + s[3] + s[4] + s[4]
-print(boss)
-
-slim = s[5] + s[8] + s[6] + s[1]
-
-print(slim)
-
-
+from random import randint
+problem_list = ['Living Gigant Tree','Monster Spirit', 'Black Dragon', 'Herobrine', 'Godzilla']
+problem = problem_list[randint(0,len(problem_list)-1)]
+print('We faced - ', problem)
+list_of_heroes = 
+[input(), input(), input()]
+print('this superheroes',)
 
 
 
@@ -36,6 +34,72 @@ print(slim)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# list_1 = [3,1,2]
+# list_2 = [4,5,6]
+# list_1.sort
+# list_1.extend(list_2)
+# print(list_1)
+# # print(max(list_1))
+# print(min(list_2))
+# print(sum(list_2))
+# list_1 = list_1+list_2
+# list = []
+# name = 'good day'
+# list.append(3)
+# list.append(name)
+# list.insert(1, 'hi')
+# list.remove('good day')
+# list[0] = 100
+# print(list)
+# s = 'impossible'
+# boss = s[7] + s[3] + s[4] + s[4]
+# print(boss)
+
+# slim = s[5] + s[8] + s[6] + s[1]
+
+# print(slim)
 # string = 'hallo world'
 # string_list = string.split() 
 # print(string_list)
