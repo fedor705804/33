fb98e1bbf355a2e0e32ca3a1700e0b781ca63f66
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-02-26 07:31:10 +0000

    tjg

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-string = 'hallo world'
-string_list = string.split() 
-print(string_list)
+s = 'impossible'
+boss = s[7] + s[3] + s[4] + s[4]
+print(boss)
 
-small_art = string[0:5]
-print(small_art)
+slim = s[5] + s[8] + s[6] + s[1]
+
+print(slim)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# string = 'hallo world'
+# string_list = string.split() 
+# print(string_list)
+
+# small_art = string[0:5]
+# print(small_art)
