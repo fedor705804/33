44323a4145ae3c1e41ec611c61d20ea09de4f688
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-03-06 09:55:34 +0000

    iteration_utilities

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from itertools import *
+from iteration_utilities import unique_justseen
 
 s = 'ttttuuuurrrrttttlllleeee'
 turtle = s[0] + s[4] + s[8] + s[12] + s[16] + s[23]
@@ -9,6 +9,8 @@ turtle = s[-24] + s[-20] + s[-16] + s[-12] + s[-8] + s[-4]
 print(turtle)
 
 turtle = unique_justseen(s)
+print(turtle)
+
 
 # slim = s[5] + s[8] + s[6] + s[1]
 
