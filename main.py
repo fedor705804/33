214be3781b2ce2d5066e8bf8b34c4bb4185faa38
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-03-07 08:01:50 +0000

    list

--- main.py
+++ main.py
@@ -1,46 +1,109 @@
-s = 'ttttuuuurrrrttttlllleeee'
-turtle = s[0] + s[4] + s[8] + s[12] + s[16] + s[23]
-print(turtle)
+from random import randint
 
-s = 'ttttuuuurrrrttttlllleeee'
-turtle = s[-24] + s[-20] + s[-16] + s[-12] + s[-8] + s[-4]  
-print(turtle)
+list = (1, 2, 3)
+print(list[0])
 
+problem_list = ['Living Gigant Tree','Monster Spirit', 'Black Dragon', 'Herobrine', 'Godzilla']
+
+problem = problem_list[randint(0,len(problem_list)-1)]
+print('We faced - ', problem)
+
+list_of_heroes = [input(), input(), input()]
+print('this superheroes',list_of_heroes, 'went a mission')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# list_1 = [3,1,2]
+# list_2 = [4,5,6]
+# list_1.sort
+# list_1.extend(list_2)
+# print(list_1)
+# # print(max(list_1))
+# print(min(list_2))
+# print(sum(list_2))
+# list_1 = list_1+list_2
+# list = []
+# name = 'good day'
+# list.append(3)
+# list.append(name)
+# list.insert(1, 'hi')
+# list.remove('good day')
+# list[0] = 100
+# print(list)
+# s = 'impossible'
+# boss = s[7] + s[3] + s[4] + s[4]
+# print(boss)
 
 # slim = s[5] + s[8] + s[6] + s[1]
 
 # print(slim)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # string = 'hallo world'
 # string_list = string.split() 
 # print(string_list)
