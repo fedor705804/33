00e9081413c1deb115508155929c532f6dace560
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-03-11 19:05:55 +0000

    union

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from random import randint
-
 our_set = {'add'}
 print(our_set)
 
@@ -9,125 +7,31 @@ print(our_set2)
 
 our_set3 = our_set2.union(our_set)
 print(our_set3)
-print(our_set2)
 
-# x = randint(0, 10)
-# print('введи число от 0 до 1000 и нажми enter')
-# list.append(input())
-
-# print('введи число от 0 до 1000 и нажми enter')
-# list.append(input())
-
-# print('введи число от 0 до 1000 и нажми enter')
-# list.append(input())
-
-# print('введи число от 0 до 1000 и нажми enter')
-# list.append(input())
-
-# print('введи число от 0 до 1000 и нажми enter')
-# list.append(input())
-
-# print(list)
-# print(list[randint(0, 4)])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# list_1 = [3,1,2]
-# list_2 = [4,5,6]
-# list_1.sort
-# list_1.extend(list_2)
-# print(list_1)
-# # print(max(list_1))
-# print(min(list_2))
-# print(sum(list_2))
-# list_1 = list_1+list_2
-# list = []
-# name = 'good day'
-# list.append(3)
-# list.append(name)
-# list.insert(1, 'hi')
-# list.remove('good day')
-# list[0] = 100
-# print(list)
-# s = 'impossible'
-# boss = s[7] + s[3] + s[4] + s[4]
-# print(boss)
-
-# slim = s[5] + s[8] + s[6] + s[1]
-
-# print(slim)
-# string = 'hallo world'
-# string_list = string.split() 
-# print(string_list)
-
-# small_art = string[0:5]
-# print(small_art)
+print('')
+print('вещи на остров')
+things1 = set()
+things1.add('еда')
+things1.add('вода')
+things1.add('телефон')
+things1.add('спички')
+things1.add('fire')
+things1.add('walker-19')
+things1.add('книжка')
+things1.add('очистка воды')
+things1.add('палатка')
+things1.add('лодка')
+print(things1)
+
+things2 = set()
+things2.add('еда')
+things2.add('леска и крючки')
+things2.add('компас')
+things2.add('топорик')
+things2.add('спички')
+print(things2)
+
+print('')
+print(things1.union(things2))
+
+print(things1.difference(things2))
