8a656a9e2b601914570d104574b6e696ae8d912b
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-03-13 07:58:28 +0000

    шаг2

--- main.py
+++ main.py
@@ -1,13 +1,82 @@
-var_1 = 2 
-var_2 = 1.1
-var_3 = 3
-var_4 = 3
-print(var_3 > var_4)
-print(var_3 >= var_4)
-print(var_1 <= var_2)
-print(var_1 == var_2)
-print(var_1 != var_2)
+question_1 = 'у стрелка на ЧАЕС тогда были галлюцинации?'
+question_2 = 'я не придуал что?'
+question_3 = 'Сахоров псих?'
+question_4 = 'да?'
+question_5 = 'я поеду в украину и поступлю в GC?'
 
+answer_1 = 'да'
+answer_2 = 'да'
+answer_3 = 'однозначно'
+answer_4 = 'нет'
+answer_5 = 'да да и да'
+
+result = 0
+
+print(question_1)
+user_answer = input()
+if user_answer == answer_1:
+
+ result += 1
+ print('ответ верный')
+
+print(question_2)
+user_answer = input()
+if user_answer == answer_2:
+
+ result += 1
+ print('ответ верный')
+
+print(question_3)
+user_answer = input()
+if user_answer == answer_3:
+
+ result += 1
+ print('ответ верный')
+
+print(question_4)
+user_answer = input()
+if user_answer == answer_4:
+
+ result += 1
+ print('ответ верный')
+
+print(question_5)
+user_answer = input()
+if user_answer == answer_5:
+
+ result += 1
+ print('ты победил урааааа!')
+
+
+print('ты ответил на',result, 'балов'   )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# var_1 = 2 
+# var_2 = 1.1
+# var_3 = 3
+# var_4 = 3
+# print(var_3 > var_4)
+# print(var_3 >= var_4)
+# print(var_1 <= var_2)
+# print(var_1 == var_2)
+# print(var_1 != var_2)
+# if v
 
 
 
