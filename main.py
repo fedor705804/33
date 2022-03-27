675fa0b085e4eb342c33adebd457b981a41652da
Fedor Dr <100030842+fedor705804@users.noreply.github.com>
2022-03-27 10:05:08 +0000

    ДЗ_к_занятию_19_03_22Цикл_for_Списки_1

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-for chih in range(0, 16, 3):
-  print('ЧИХ=' + str(chih))
-# print('мы вышли из цикла')
+for i in range(99999999999999999999999999999999999):
 
-print(chr(10))
-  
-chih = 0
-while chih < 16:
-  print('ЧИХ №' + str(chih))
-  chih = chih + 3
- # break
+ question_1 = 'кто нанял наёмников на уничтожение свободы? Долг или О-сознание'
+ question_2 = 'как звали из книги Василия Орехова пьяного умника? Рентген или Хемуль?'
+ question_3 = 'сколько удалёных мутантов в S.T.A.L.K.E.R.? 20 или 30'
+
+ answer_1 = 'Долг'
+ answer_2 = 'Рентген'
+ answer_3 = '30'
+
+ result = 0
+
+ print(question_1)
+ user_answer = input()
+ if user_answer == answer_1:
+  result += 1
+  print('ответ верный')
+
+ print(question_2)
+ user_answer = input()
+ if user_answer == answer_2:
+  result += 1
+  print('ответ верный')
+
+ print(question_3)
+ user_answer = input()
+ if user_answer == answer_3:
+  result += 1
+  print('ответ верный')
+
+ print('хотите пройти игру заново')
+ print('0 - да, 1 - нет')
+ last_answer = int(input('Введите ответ: '))
+ if last_answer == 1:
+  break
+
+# print('ты ответил на',result, 'балов'   )
